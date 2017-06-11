19dfda4cd1135d32fb25c384fa3fde131afb563b
Danilo Bargen <mail@dbrgn.ch>
2017-06-12 00:22:55 +0200

    Start work on first API example

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,10 +2,11 @@ extern crate gerber;
 
 use gerber::Layer;
 use gerber::types::{FileAttribute, Part, FileFunction, ExtendedPosition, CopperType};
-//use gerber::{Aperture, Circle, Rectangular};
+use gerber::types::{CoordinateFormat, Aperture, Circle, Rectangular};
 
 fn main() {
-    let mut layer = Layer::new();
+    let coordinate_format = CoordinateFormat::new(6, 6);
+    let mut layer = Layer::new(coordinate_format);
 
     // Set attributes
     layer.set_file_attribute(FileAttribute::Part(Part::Single));
@@ -15,20 +16,23 @@ fn main() {
         copper_type: Some(CopperType::Signal),
     }));
 
-    //// Create apertures for traces
-    //let trace_power = layer.create_aperture(Aperture::Circle(Circle::new(1.0)));
-    //
-    //// Create apertures for pads
-    //let pad_round = layer.create_aperture(Aperture::Circle(Circle::with_hole(1.2, 0.8)));
-    //let pad_square = layer.create_aperture(Aperture::Rectangle(Rectangular::with_hole(1.2, 1.2, 0.8)));
-    //
-    //// Flash pads
-    //layer.flash((10.0, 10.0), pad_round);
-    //layer.flash((20.0, 10.0), pad_round);
-    //layer.flash((30.0, 10.0), pad_square);
-    //
+    // Create apertures for traces
+    let trace_power = layer.create_aperture(Aperture::Circle(Circle::new(1.0)));
+
+    // Create apertures for pads
+    let pad_round = layer.create_aperture(Aperture::Circle(Circle::with_hole(1.2, 0.8)));
+    let pad_square = layer.create_aperture(Aperture::Rectangle(Rectangular::with_hole(1.2, 1.2, 0.8)));
+
+    // Flash pads
+    layer.flash((10.0, 10.0), &pad_round);
+    layer.flash((20.0, 10.0), &pad_round);
+    layer.flash((30.0, 10.0), &pad_square);
+
     //// Draw trace connecting all pads
     //layer.draw_line((10.0, 10.0), (30.0, 10.0), trace_power);
 
     println!("{:?}", layer);
+
+    println!("\nGerber code:\n");
+    println!("{}", layer.to_code());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,123 @@
+extern crate conv;
 pub extern crate gerber_types as types;
+
+use std::io::{BufWriter, Write};
+use std::rc::Rc;
+
+use conv::TryFrom;
+use types::GerberCode;
+
 
 #[derive(Debug)]
 pub struct Layer {
     file_attributes: Vec<types::FileAttribute>,
+    apertures: Vec<Rc<types::ApertureDefinition>>,
+    coordinate_format: types::CoordinateFormat,
+    commands: Vec<types::Command>,
 }
 
 impl Layer {
-    pub fn new() -> Self {
+    pub fn new(coordinate_format: types::CoordinateFormat) -> Self {
         Layer {
             file_attributes: vec![],
+            apertures: vec![],
+            coordinate_format: coordinate_format,
+            commands: vec![],
         }
     }
 
     pub fn set_file_attribute(&mut self, attribute: types::FileAttribute) {
         self.file_attributes.push(attribute);
+    }
+
+    /// Create a new aperture definition.
+    pub fn create_aperture(&mut self, aperture: types::Aperture) -> Rc<types::ApertureDefinition> {
+        let aperture_definition = Rc::new(types::ApertureDefinition {
+            code: 10 + self.apertures.len() as i32,
+            aperture: aperture,
+        });
+        self.apertures.push(aperture_definition.clone());
+        aperture_definition
+    }
+
+    /// Flash the specified aperture at the specified coordinates.
+    ///
+    /// This function will panic if one of the coordinates is `NaN` or
+    /// `Infinite`.
+    pub fn flash(&mut self, coords: (f64, f64), aperture: &types::ApertureDefinition) {
+        // TODO: Optimize this so that SelectAperture is not used multiple times.
+        self.commands.push(
+            types::FunctionCode::DCode(
+                types::DCode::SelectAperture(aperture.code)
+            ).into()
+        );
+        self.commands.push(
+            types::FunctionCode::DCode(
+                types::DCode::Operation(
+                    types::Operation::Flash(
+                        types::Coordinates::new(
+                            types::CoordinateNumber::try_from(coords.0).unwrap(),
+                            types::CoordinateNumber::try_from(coords.1).unwrap(),
+                            self.coordinate_format
+                        )
+                    )
+                )
+            ).into(),
+        );
+    }
+
+    /// Generate the Gerber code for this layer.
+    ///
+    /// Will panic if the gerber-types-rs library generates invalid UTF-8 data
+    /// (which is always a bug in that library).
+    pub fn to_code(&self) -> String {
+        let mut writer = BufWriter::new(vec![]);
+
+        // Write attributes
+        // TODO: This is unelegant
+        &self.file_attributes
+            .iter()
+            .map(|a| {
+                types::Command::ExtendedCode(
+                    types::ExtendedCode::FileAttribute(
+                        a.to_owned()
+                    )
+                )
+            })
+            .collect::<Vec<_>>()
+            .to_code(&mut writer)
+            .unwrap();
+        write!(writer, "\n").unwrap();
+
+        // Write apertures
+        &self.apertures
+            .iter()
+            .cloned()
+            .map(|mut a| {
+                types::Command::ExtendedCode(
+                    types::ExtendedCode::ApertureDefinition(
+                        Rc::make_mut(&mut a).to_owned()
+                    )
+                )
+            })
+            .collect::<Vec<_>>()
+            .to_code(&mut writer)
+            .unwrap();
+        write!(writer, "\n").unwrap();
+
+        // Write commands
+        self.commands.to_code(&mut writer).unwrap();
+        write!(writer, "\n").unwrap();
+
+        // Write end of file
+        types::Command::FunctionCode(
+            types::FunctionCode::MCode(
+                types::MCode::EndOfFile
+            ),
+        ).to_code(&mut writer).unwrap();
+        write!(writer, "\n").unwrap();
+
+        String::from_utf8(writer.into_inner().unwrap()).unwrap()
     }
 }
 
